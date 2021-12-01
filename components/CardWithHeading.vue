074e1640eb58fb9b7825d4c3c3cd5a40898e72fc
Yasmani <yasmany.07@gmail.com>
2021-11-30 22:01:10 -0500

    feat: add card component

---
+++ components/CardWithHeading.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="max-w-7xl mx-auto py-4 sm:px-6 lg:px-8">
+    <div class="max-w-none mx-auto">
+      <div class="bg-white overflow-hidden rounded-lg shadow">
+        <slot name="headingArea">
+          <p>Heading content goes here</p>
+        </slot>
+        <!-- List content -->
+        <slot name="contentArea">
+          <p>Content goes here</p>
+        </slot>
+        <!-- /End list content -->
+      </div>
+    </div>
+  </div>
+</template>
