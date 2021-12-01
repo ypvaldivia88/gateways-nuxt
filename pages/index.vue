0299e3259f653bd35534aae9acc5df78309c582d
Yasmani <yasmany.07@gmail.com>
2021-11-30 22:02:08 -0500

    feat: add card

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,288 @@
 <template>
-  <div>Hello World!</div>
+  <div class="py-8">
+    <CardWithHeading>
+      <template #headingArea>
+        <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+          <div
+            class="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-no-wrap"
+          >
+            <div class="ml-4 mt-2">
+              <h3 class="text-lg leading-6 font-medium text-gray-900">
+                Recent Estimates
+              </h3>
+            </div>
+            <div class="ml-4 mt-2 flex-shrink-0">
+              <span class="inline-flex rounded-md shadow-sm">
+                <nuxt-link to="/estimate/ClaimInfo">
+                  <button
+                    type="button"
+                    class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700"
+                  >
+                    Create new
+                  </button>
+                </nuxt-link>
+              </span>
+            </div>
+          </div>
+        </div>
+      </template>
+      <template #contentArea>
+        <ul>
+          <li>
+            <a
+              href="#"
+              class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out"
+            >
+              <div class="flex items-center px-4 py-4 sm:px-6">
+                <div class="min-w-0 flex-1 flex items-center">
+                  <div class="flex-shrink-0">
+                    <img
+                      class="h-12 w-12 rounded-full"
+                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      alt=""
+                    />
+                  </div>
+                  <div
+                    class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4"
+                  >
+                    <div>
+                      <div
+                        class="text-sm leading-5 font-medium text-indigo-600 truncate"
+                      >
+                        Ricardo Cooper
+                      </div>
+                      <div
+                        class="mt-2 flex items-center text-sm leading-5 text-gray-500"
+                      >
+                        <svg
+                          class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"
+                          />
+                          <path
+                            d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"
+                          />
+                        </svg>
+                        <span class="truncate"
+                          >ricardo.cooper@example.com
+                        </span>
+                      </div>
+                    </div>
+                    <div class="hidden md:block">
+                      <div>
+                        <div class="text-sm leading-5 text-gray-900">
+                          Applied on
+                          <time datetime="2020-01-07">January 7, 2020 </time>
+                        </div>
+                        <div
+                          class="mt-2 flex items-center text-sm leading-5 text-gray-500"
+                        >
+                          <svg
+                            class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                          Completed phone screening
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <svg
+                    class="h-5 w-5 text-gray-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li class="border-t border-gray-200">
+            <a
+              href="#"
+              class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out"
+            >
+              <div class="flex items-center px-4 py-4 sm:px-6">
+                <div class="min-w-0 flex-1 flex items-center">
+                  <div class="flex-shrink-0">
+                    <img
+                      class="h-12 w-12 rounded-full"
+                      src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      alt=""
+                    />
+                  </div>
+                  <div
+                    class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4"
+                  >
+                    <div>
+                      <div
+                        class="text-sm leading-5 font-medium text-indigo-600 truncate"
+                      >
+                        Kristen Ramos
+                      </div>
+                      <div
+                        class="mt-2 flex items-center text-sm leading-5 text-gray-500"
+                      >
+                        <svg
+                          class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"
+                          />
+                          <path
+                            d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"
+                          />
+                        </svg>
+                        <span class="truncate">kristen.ramos@example.com </span>
+                      </div>
+                    </div>
+                    <div class="hidden md:block">
+                      <div>
+                        <div class="text-sm leading-5 text-gray-900">
+                          Applied on
+                          <time datetime="2020-01-07">January 7, 2020 </time>
+                        </div>
+                        <div
+                          class="mt-2 flex items-center text-sm leading-5 text-gray-500"
+                        >
+                          <svg
+                            class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                          Completed phone screening
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <svg
+                    class="h-5 w-5 text-gray-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li class="border-t border-gray-200">
+            <a
+              href="#"
+              class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out"
+            >
+              <div class="flex items-center px-4 py-4 sm:px-6">
+                <div class="min-w-0 flex-1 flex items-center">
+                  <div class="flex-shrink-0">
+                    <img
+                      class="h-12 w-12 rounded-full"
+                      src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      alt=""
+                    />
+                  </div>
+                  <div
+                    class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4"
+                  >
+                    <div>
+                      <div
+                        class="text-sm leading-5 font-medium text-indigo-600 truncate"
+                      >
+                        Ted Fox
+                      </div>
+                      <div
+                        class="mt-2 flex items-center text-sm leading-5 text-gray-500"
+                      >
+                        <svg
+                          class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                        >
+                          <path
+                            d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"
+                          />
+                          <path
+                            d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"
+                          />
+                        </svg>
+                        <span class="truncate">ted.fox@example.com </span>
+                      </div>
+                    </div>
+                    <div class="hidden md:block">
+                      <div>
+                        <div class="text-sm leading-5 text-gray-900">
+                          Applied on
+                          <time datetime="2020-01-07">January 7, 2020 </time>
+                        </div>
+                        <div
+                          class="mt-2 flex items-center text-sm leading-5 text-gray-500"
+                        >
+                          <svg
+                            class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                          Completed phone screening
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <svg
+                    class="h-5 w-5 text-gray-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </template>
+    </CardWithHeading>
+  </div>
 </template>
 
 <script>
